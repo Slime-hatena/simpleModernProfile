70a97785e47f1230cefa1f5d076098228a2d8324
Slime_hatena <Slime-hatena@users.noreply.github.com>
2017-08-07 03:55:23 +0900

    メニューっぽいのの動きを実装

--- js/main.js
+++ js/main.js
@@ -9,8 +9,31 @@ $(document).ready(function() {
         $("div#field-bio").children('span').text(data["bio"]);
         $("div#field-location").children('span').text(data["location"]);
         $("div#field-mail").children('span').text(data["mail"]);
-        $("div#field-url").children('span').text(data["url"]);
+        $("div#field-url").children('a').children('span').text(data["url"]);
+        $("div#field-url").children('a').attr('href', data["url"]);
         $("div#field-birthdate").children('span').text(data["birthdate"]);
 
+    });
+});
+
+$(function() {
+    $(window).scroll(function() {
+        var ScrTop = $(document).scrollTop();
+        if (ScrTop > 50) {
+            $('#fixedMenu').css({ 'position': 'fixed', 'top': '0' });
+            if (!$.support.noCloneChecked) {
+                $('#fixedMenu').css('position', 'absolute')
+                    .css('top', ($(document).scrollTop()) + "px");
+            }
+            $('#fixedPagetop').css({ 'display': 'block' });
+        }
+        if (ScrTop < 50) {
+            $('#fixedMenu').css({ 'position': 'absolute', 'top': '50px' });
+            $('#fixedPagetop').css({ 'display': 'none' });
+        }
+    });
+    $('#fixedPagetop a').click(function() {
+        $('html,body').animate({ scrollTop: 0 }, 1000);
+        return false;
     });
 });
