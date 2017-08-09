d4aacb9299f92323fb713c7f937705cf2040594c
Slime_hatena <Slime-hatena@users.noreply.github.com>
2017-08-10 03:34:43 +0900

    プラグインを複数追加

---
+++ plugins/linkPresent/section.js
@@ -0,0 +1,31 @@
+$.getJSON("plugins/linkPresent/option.json", function(data) {
+    $.each(data, function(index, val) {
+        var str = index;
+        var isName = (val['name'] !== false ? true : false);
+        var isIcon = (val['icon'] !== false ? true : false);
+        var isLink = (val['link'] !== false ? true : false);
+
+        str = "<div>" + (isIcon ? val['icon'] : "□") +
+            (isLink ? '<a href="' + val['link'] + '" target="_blank">' : "") +
+            index +
+            (isName ? ":  " + val['name'] : "") +
+            (isLink ? '</a>' : "") + "</div>";
+
+        /*
+                if (val['name'] !== false) {
+                    str += ": " + val['name'];
+                }
+
+                if (val['icon'] !== false) {
+                    str = val['icon'] + str;
+                }
+
+                if (val['link'] !== false) {
+                    str = '<div>< + str + ' < /a></div > ';
+                }
+                */
+
+        $("#socialProfileAppend").append(str);
+
+    });
+});
