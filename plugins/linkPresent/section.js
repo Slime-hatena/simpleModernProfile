7df424d6423be44e783bd014d34a7c1c4d4ed686
Slime_hatena <Slime-hatena@users.noreply.github.com>
2017-08-10 18:03:22 +0900

    linkPresentにサムネイルを追加

--- plugins/linkPresent/section.js
+++ plugins/linkPresent/section.js
@@ -2,7 +2,7 @@ $.getJSON("plugins/linkPresent/option.json", function(data) {
     $.each(data['section'], function(index, val) {
         str = "<h3>" + val['title'] + "</h3><div>" +
             '<a href="' + val['url'] + '" target="_blank">' +
-            val['url'] + '</a><p>' + val['description'] + '</p></div>';
+            val['url'] + '</a><img src="http://mozshot.nemui.org/shot/large?' + val['url'] + '"><p>' + val['description'] + '</p></div>';
         $(".linkPresent").append(str);
     });
 });
